dd4c513870486ad2fd79b4254626a75ef380db1b
Ahmed Elhady <ahmedmohamed123217@gmail.com>
2022-10-23 11:43:47 +0200

    built models & relationships

--- task-3/student/models.py
+++ task-3/student/models.py
@@ -1,3 +1,48 @@
+from asyncio import constants
+from tkinter.tix import Tree
 from django.db import models
 
 # Create your models here.
+
+
+class Parent(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length =100)
+    last_name = models.CharField(max_length =100)
+
+    def __str__(self):
+        return self.first_name
+    class Meta:
+        db_table = 'parent'
+
+class Subject(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length =100)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        db_table = 'subject'
+
+
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length =100)
+    last_name = models.CharField(max_length =100)
+    class_number = models.IntegerField()
+    age = models.IntegerField()
+    email = models.EmailField(unique = True)
+
+    parent = models.ForeignKey(Parent,related_name="students",null=True,on_delete=models.SET_NULL)
+    subject = models.ManyToManyField(Subject,related_name="students")
+
+    constraints = [
+        models.CheckConstraint(
+            name = 'age greater than 18',check=models.Q(age__gt=5)
+        ),
+    ]
+
+    def __str__(self):
+        return self.first_name
+    class Meta:
+        db_table = 'student'
--- task-3/student/views.py
+++ task-3/student/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse, HttpResponse
+from .models import Student, Parent, Subject
+import json
 
 # Create your views here.
+
+class StudentV(View):
+    def get(request,*args, **kwargs):
+        return HttpResponse("hello world")
+
+    pass
+
+class StudentID(View):
+    pass
